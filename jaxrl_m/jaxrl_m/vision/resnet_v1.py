4794668ef51d58aa58b26abe30d96c6500626da3
derek <derekguo@berkeley.edu>
2024-03-21 02:06:36 +0000

    add jaxrl_m

---
+++ jaxrl_m/jaxrl_m/vision/resnet_v1.py
@@ -0,0 +1,219 @@
+"""Flax implementation of ResNet V1. Stolen from tps://github.com/google/flax/blob/master/examples/imagenet/models.py"""
+from functools import partial
+from typing import Any, Callable, Sequence, Tuple
+
+from flax import linen as nn
+import jax.numpy as jnp
+
+ModuleDef = Any
+
+
+class ResNetBlock(nn.Module):
+  """ResNet block."""
+  filters: int
+  conv: ModuleDef
+  norm: ModuleDef
+  act: Callable
+  strides: Tuple[int, int] = (1, 1)
+
+  @nn.compact
+  def __call__(self, x,):
+    residual = x
+    y = self.conv(self.filters, (3, 3), self.strides, padding=((1, 1), (1, 1)))(x)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters, (3, 3), padding=((1, 1), (1, 1)))(y)
+    y = self.norm(scale_init=nn.initializers.zeros)(y)
+
+    if residual.shape != y.shape:
+      residual = self.conv(self.filters, (1, 1),
+                           self.strides, name='conv_proj')(residual)
+      residual = self.norm(name='norm_proj')(residual)
+
+    return self.act(residual + y)
+
+
+class BottleneckResNetBlock(nn.Module):
+  """Bottleneck ResNet block."""
+  filters: int
+  conv: ModuleDef
+  norm: ModuleDef
+  act: Callable
+  strides: Tuple[int, int] = (1, 1)
+
+  @nn.compact
+  def __call__(self, x):
+    residual = x
+    y = self.conv(self.filters, (1, 1))(x)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters, (3, 3), self.strides, padding=((1, 1), (1, 1)))(y)
+    y = self.norm()(y)
+    y = self.act(y)
+    y = self.conv(self.filters * 4, (1, 1))(y)
+    y = self.norm(scale_init=nn.initializers.zeros)(y)
+
+    if residual.shape != y.shape:
+      residual = self.conv(self.filters * 4, (1, 1),
+                           self.strides, name='conv_proj')(residual)
+      residual = self.norm(name='norm_proj')(residual)
+
+    return self.act(residual + y)
+
+
+class ResNet(nn.Module):
+  """ResNetV1."""
+  stage_sizes: Sequence[int]
+  block_cls: ModuleDef
+  num_filters: int = 64
+  dtype: Any = jnp.float32
+  act: Callable = nn.relu
+  output_fc_dim: int = None
+
+  @nn.compact
+  def __call__(self, x, train: bool = True):
+
+    conv = partial(nn.Conv, use_bias=False, dtype=self.dtype)
+    norm = partial(nn.BatchNorm,
+                   use_running_average=not train,
+                   momentum=0.9,
+                   epsilon=1e-5,
+                   dtype=self.dtype)
+
+    x = conv(self.num_filters, (7, 7), (2, 2),
+             padding=[(3, 3), (3, 3)],
+             name='conv_init')(x)
+    x = norm(name='bn_init')(x)
+    x = nn.relu(x)
+    x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=((1, 1), (1, 1)))
+    for i, block_size in enumerate(self.stage_sizes):
+      for j in range(block_size):
+        strides = (2, 2) if i > 0 and j == 0 else (1, 1)
+        x = self.block_cls(self.num_filters * 2 ** i,
+                           strides=strides,
+                           conv=conv,
+                           norm=norm,
+                           act=self.act)(x)
+    x = jnp.mean(x, axis=(1, 2))
+    if self.output_fc_dim is not None:
+      x = nn.Dense(self.output_fc_dim, dtype=self.dtype)(x)
+
+    # x = nn.Dense(self.num_classes, dtype=self.dtype)(x)
+    # x = jnp.asarray(x, self.dtype)
+    # # x = nn.log_softmax(x)  # to match the Torch implementation at https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+    return x
+
+
+
+# function to translate pytorch param keys to jax:
+def translate_key(pytorch_name, resnet="50"):
+    if resnet == "50":
+      block_name = "BottleneckResNetBlock" 
+      layer_list = [3, 4, 6, 3]
+    elif resnet == "18":
+      block_name = "ResNetBlock"
+      layer_list = [2, 2, 2, 2]
+    else:
+      raise RuntimeError("Choose one of {'18', '50'}.")
+
+    split = pytorch_name.split('.')
+    
+    # fc.{weight|bias} -> (params, Dense_0, {kernel|bias})
+    if len(split) == 2 and split[0] == 'fc':
+      return ("params", "Dense_0", "bias" if split[1] == "bias" else "kernel")
+
+    # layer{i}.{j}.bn{k}.{weight|bias|running_mean|running_var} -> ({params|batch_stats}, BottleneckBlock_{}, BatchNorm_{}, {scale|bias|mean|var})
+    if len(split) == 4 and split[0][:-1] == 'layer' and split[1].isdigit() and split[2][:-1] == 'bn':
+      if split[3] in ['num_batches_tracked']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params" if split[3] in ["weight", "bias"] else "batch_stats",
+              f"{block_name}_{sum(layer_list[:int(split[0][-1]) - 1]) + int(split[1])}",
+              f"BatchNorm_{int(split[2][-1]) - 1}",
+              "scale" if split[3] == "weight" else split[3][8:] if split[3] in ["running_mean", "running_var"] else "bias")
+
+    # layer{i}.{j}.conv{k}.weight -> (params, BottleneckBlock_{}, Conv_{}, kernel)
+    if len(split) == 4 and split[0][:-1] == 'layer' and split[1].isdigit() and split[2][:-1] == 'conv':
+      if split[3] in ['bias']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params",
+              f"{block_name}_{sum(layer_list[:int(split[0][-1]) - 1]) + int(split[1])}",
+              f"Conv_{int(split[2][-1]) - 1}",
+              "kernel")
+
+    # bn1.{weight|bias|running_mean|running_var} -> ({params|batch_stats}, bn_init, {scale|bias|mean|var})
+    if len(split) == 2 and split[0] == "bn1":
+      if split[1] in ['num_batches_tracked']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params" if split[1] in ["weight", "bias"] else "batch_stats",
+              "bn_init",
+              "scale" if split[1] == "weight" else split[1][8:] if split[1] in ["running_mean", "running_var"] else "bias")
+
+    # conv1.weight -> (params, conv_init, kernel)
+    if len(split) == 2 and split[0] == "conv1":
+      if split[1] in ['bias']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params", "conv_init", "kernel")
+
+    # layer{i}.{j}.downsample.0.weight -> (params, BottleneckBlock_{}, conv_proj, kernel)
+    if len(split) == 5 and split[0][:-1] == 'layer' and split[1].isdigit() and split[2] == 'downsample' and split[3] == '0':
+      if split[4] in ['bias']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params",
+              f"{block_name}_{sum(layer_list[:int(split[0][-1]) - 1]) + int(split[1])}",
+              "conv_proj",
+              "kernel")
+
+    # layer{i}.{j}.downsample.1.{weight|bias|running_mean|running_var} -> ({params|batch_stats}, BottleneckBlock_{}, norm_proj, {scale|bias|mean|var})
+    if len(split) == 5 and split[0][:-1] == 'layer' and split[1].isdigit() and split[2] == 'downsample' and split[3] == '1':
+      if split[4] in ['num_batches_tracked']:
+        print(f"NO PATTERN MATCHES: {pytorch_name}")
+        return None
+
+      return ("params" if split[4] in ["weight", "bias"] else "batch_stats",
+              f"{block_name}_{sum(layer_list[:int(split[0][-1]) - 1]) + int(split[1])}",
+              "norm_proj",
+              "scale" if split[4] == "weight" else split[4][8:] if split[4] in ["running_mean", "running_var"] else "bias")
+
+
+    print(f"NO PATTERN MATCHES: {pytorch_name}")
+    return None
+
+def convert_pytorch_to_jax(pytorch_statedict, jax_variables, resnet_type="50"):
+  from flax.traverse_util import flatten_dict, unflatten_dict
+  from flax.core import freeze, unfreeze
+
+
+  jax_params = flatten_dict(unfreeze(jax_variables))
+  # create a new dict the same shape as the original jax params dict but filled with the (transposed) pytorch weights
+  jax2pytorch = {translate_key(key, resnet_type): key for key in pytorch_statedict.keys() if translate_key(key) is not None}
+  pytorch_params = {k: v.numpy().T if len(v.shape) != 4 else v.numpy().transpose((2, 3, 1, 0))
+                    for k, v in pytorch_statedict.items()}
+  new_jax_params = freeze(unflatten_dict({key: pytorch_params[jax2pytorch[key]] for key in jax_params.keys()}))
+  return new_jax_params
+
+vanilla_resnetv1_configs = {
+    'resnetv1-18': partial(ResNet, stage_sizes=[2, 2, 2, 2],
+                   block_cls=ResNetBlock),
+    'resnetv1-34': partial(ResNet, stage_sizes=[3, 4, 6, 3],
+                   block_cls=ResNetBlock),
+    'resnetv1-50': partial(ResNet, stage_sizes=[3, 4, 6, 3],
+                   block_cls=BottleneckResNetBlock),
+    'resnetv1-101': partial(ResNet, stage_sizes=[3, 4, 23, 3],
+                    block_cls=BottleneckResNetBlock),
+    'resnetv1-152': partial(ResNet, stage_sizes=[3, 8, 36, 3],
+                    block_cls=BottleneckResNetBlock),
+    'resnetv1-200': partial(ResNet, stage_sizes=[3, 24, 36, 3],
+                    block_cls=BottleneckResNetBlock)
+}
+
+vanilla_resnetv1_configs['vip'] = partial(vanilla_resnetv1_configs['resnetv1-50'], output_fc_dim=1024)
---
+++ jaxrl_m/jaxrl_m/vision/resnet_v2.py
@@ -0,0 +1,216 @@
+# Copyright 2022 Big Vision Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""BiT models as in the paper (ResNet V2) w/ loading of public weights.
+
+See reproduction proof: http://(internal link)
+"""
+
+import functools
+import re
+from typing import Optional, Sequence, Union
+
+import flax.linen as nn
+import jax.numpy as jnp
+import jax
+from icecream import ic
+
+def standardize(x, axis, eps):
+  x = x - jnp.mean(x, axis=axis, keepdims=True)
+  x = x / jnp.sqrt(jnp.mean(jnp.square(x), axis=axis, keepdims=True) + eps)
+  return x
+
+
+# Defined our own, because we compute normalizing variance slightly differently,
+# which does affect performance when loading pre-trained weights!
+class GroupNorm(nn.Module):
+  """Group normalization (arxiv.org/abs/1803.08494)."""
+  ngroups: int = 32
+
+  @nn.compact
+  def __call__(self, x):
+
+    input_shape = x.shape
+    group_shape = x.shape[:-1] + (self.ngroups, x.shape[-1] // self.ngroups)
+
+    x = x.reshape(group_shape)
+
+    # Standardize along spatial and group dimensions
+    x = standardize(x, axis=[1, 2, 4], eps=1e-5)
+    x = x.reshape(input_shape)
+
+    bias_scale_shape = tuple([1, 1, 1] + [input_shape[-1]])
+    x = x * self.param('scale', nn.initializers.ones, bias_scale_shape)
+    x = x + self.param('bias', nn.initializers.zeros, bias_scale_shape)
+    return x
+
+# adapted from https://github.com/google-research/robotics_transformer/blob/master/film_efficientnet/film_conditioning_layer.py
+class FilmConditioning(nn.Module):
+    @nn.compact
+    def __call__(self, conv_filters: jnp.ndarray, conditioning: jnp.ndarray):
+        """Applies FiLM conditioning to a convolutional feature map.
+
+        Args:
+            conv_filters: A tensor of shape [batch_size, height, width, channels].
+            conditioning: A tensor of shape [batch_size, conditioning_size].
+
+        Returns:
+            A tensor of shape [batch_size, height, width, channels].
+        """
+        assert conditioning is not None, "FiLM conditioning requires input."
+        projected_cond_add = nn.Dense(
+            features=conv_filters.shape[-1],
+            kernel_init=nn.initializers.zeros,
+            bias_init=nn.initializers.zeros,
+        )(conditioning)
+        projected_cond_mult = nn.Dense(
+            features=conv_filters.shape[-1],
+            kernel_init=nn.initializers.zeros,
+            bias_init=nn.initializers.zeros,
+        )(conditioning)
+
+        projected_cond_add = projected_cond_add[:, None, None, :]
+        projected_cond_mult = projected_cond_mult[:, None, None, :]
+
+        return conv_filters * (1 + projected_cond_add) + projected_cond_mult
+
+
+class StdConv(nn.Conv):
+
+  def param(self, name, *a, **kw):
+    param = super().param(name, *a, **kw)
+    if name == 'kernel':
+      param = standardize(param, axis=[0, 1, 2], eps=1e-10)
+    return param
+
+
+class RootBlock(nn.Module):
+  """Root block of ResNet."""
+  width: int
+
+  @nn.compact
+  def __call__(self, x):
+    x = StdConv(self.width, (7, 7), (2, 2), padding=[(3, 3), (3, 3)],
+                use_bias=False, name='conv_root')(x)
+    x = nn.max_pool(x, (3, 3), strides=(2, 2), padding=[(1, 1), (1, 1)])
+    return x
+
+
+class ResidualUnit(nn.Module):
+  """Bottleneck ResNet block."""
+  nmid: Optional[int] = None
+  strides: Sequence[int] = (1, 1)
+
+  @nn.compact
+  def __call__(self, x):
+    nmid = self.nmid or x.shape[-1] // 4
+    nout = nmid * 4
+    conv = functools.partial(StdConv, use_bias=False)
+
+    residual = x
+    x = GroupNorm(name='gn1')(x)
+    x = nn.relu(x)
+
+    if x.shape[-1] != nout or self.strides != (1, 1):
+      residual = conv(nout, (1, 1), self.strides, name='conv_proj')(x)
+
+    x = conv(nmid, (1, 1), name='conv1')(x)
+    x = GroupNorm(name='gn2')(x)
+    x = nn.relu(x)
+    x = conv(nmid, (3, 3), self.strides, padding=[(1, 1), (1, 1)],
+             name='conv2')(x)
+    x = GroupNorm(name='gn3')(x)
+    x = nn.relu(x)
+    x = conv(nout, (1, 1), name='conv3')(x)
+
+    return x + residual
+
+
+class ResNetStage(nn.Module):
+  """A stage (sequence of same-resolution blocks)."""
+  block_size: int
+  nmid: Optional[int] = None
+  first_stride: Sequence[int] = (1, 1)
+
+  @nn.compact
+  def __call__(self, x):
+    out = {}
+    x = out['unit01'] = ResidualUnit(
+        self.nmid, strides=self.first_stride, name='unit01')(x)
+    for i in range(1, self.block_size):
+      x = out[f'unit{i+1:02d}'] = ResidualUnit(
+          self.nmid, name=f'unit{i+1:02d}')(x)
+    return x, out
+
+
+class Model(nn.Module):
+  """ResNetV2."""
+  num_classes: int
+  width: int = 1
+  depth: Union[int, Sequence[int]] = 50  # 5/101/152, or list of block depths.
+  image_shape: tuple = None
+  use_film_conditioning: bool = False
+  raw_input: bool = True
+
+  @nn.compact
+  def __call__(self, x, *, train=False, film_conditioning=None):
+    if self.raw_input:
+      x = x / 255.0
+    blocks = get_block_desc(self.depth)
+    width = int(64 * self.width)
+    out = {}
+
+    x = out['stem'] = RootBlock(width=width, name='root_block')(x)
+
+    # Blocks
+    x, out['stage1'] = ResNetStage(blocks[0], nmid=width, name='block1')(x)
+    if self.use_film_conditioning:
+      x = FilmConditioning()(x, film_conditioning)
+    for i, block_size in enumerate(blocks[1:], 1):
+      x, out[f'stage{i + 1}'] = ResNetStage(
+          block_size, width * 2 ** i,
+          first_stride=(2, 2), name=f'block{i + 1}')(x)
+      if self.use_film_conditioning:
+        x = FilmConditioning()(x, film_conditioning)
+
+    # Pre-head
+    x = out['norm_pre_head'] = GroupNorm(name='norm-pre-head')(x)
+    x = out['pre_logits_2d'] = nn.relu(x)
+    x = out['pre_logits'] = jnp.mean(x, axis=(1, 2))
+
+    # Head
+    if self.num_classes:
+      head = nn.Dense(self.num_classes, name='head',
+                      kernel_init=nn.initializers.zeros)
+      out['logits_2d'] = head(out['pre_logits_2d'])
+      x = out['logits'] = head(out['pre_logits'])
+
+    return x#, out
+
+def get_block_desc(depth):
+  if isinstance(depth, list):  # Be robust to silly mistakes.
+    depth = tuple(depth)
+  return {
+      26: [2, 2, 2, 2],  # From timm, gets ~75% on ImageNet.
+      50: [3, 4, 6, 3],
+      101: [3, 4, 23, 3],
+      152: [3, 8, 36, 3],
+      200: [3, 24, 36, 3]
+  }.get(depth, depth)
+
+import functools as ft
+resnetv2_configs = {
+  'resnetv2-26-1': ft.partial(Model, num_classes=None, depth=26),
+  'resnetv2-50-1': ft.partial(Model, num_classes=None, depth=50),
+}
